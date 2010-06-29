336381361163c0522439b3c85e3bfa99e9e50b9b
mangano <>
2010-06-29 14:37:36 +0000

    add IP selector

---
+++ Filters/src/MuIPSelector.cc
@@ -0,0 +1,61 @@
+#include "FWCore/PluginManager/interface/ModuleDef.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "CommonTools/UtilAlgos/interface/ObjectSelector.h"
+
+#include "CommonTools/RecoAlgos/interface/MuonSelector.h"
+
+#include <DataFormats/VertexReco/interface/Vertex.h>
+#include <DataFormats/VertexReco/interface/VertexFwd.h>
+
+
+struct MuIPSelector {
+  typedef reco::MuonCollection collection;
+
+  typedef std::vector<const reco::Muon *> container;
+
+  typedef container::const_iterator const_iterator;
+
+  MuIPSelector ( const edm::ParameterSet & cfg ) :
+    d0Cut_( cfg.getParameter<double>( "d0Cut" ) )
+  { }
+
+  const_iterator begin() const { return selected_.begin(); }
+  const_iterator end() const { return selected_.end(); }
+
+
+  void select( const edm::Handle<collection> & c, 
+	       const edm::Event & event, 
+	       const edm::EventSetup& eventSetup) 
+  {
+    using namespace edm;
+    using namespace reco;
+    
+    Handle<VertexCollection> vertices;
+    event.getByLabel("offlinePrimaryVertices",vertices);
+
+    Vertex::Point pvPos = vertices->front().position();
+
+    selected_.clear();
+    for( reco::MuonCollection::const_iterator mu = c->begin(); 
+         mu != c->end(); ++ mu )
+      if ( mu->isGlobalMuon()         &&
+	   abs( mu->globalTrack()->dxy(pvPos) ) < d0Cut_
+	   ) selected_.push_back( & * mu );
+  }
+  size_t size() const { return selected_.size(); }
+
+private:
+  container selected_;
+  double d0Cut_;
+};
+
+
+
+
+
+
+// define your producer name
+typedef ObjectSelector<MuIPSelector> MuIPSelection;
+
+// declare the module as plugin
+DEFINE_FWK_MODULE( MuIPSelection );
