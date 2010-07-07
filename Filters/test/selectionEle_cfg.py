7d2943e2970f5d70be12726cace7cc02309186c8
mangano <>
2010-07-07 15:57:52 +0000

    use skimmed files from Giovanni as input for tests

--- Filters/test/selectionEle_cfg.py
+++ Filters/test/selectionEle_cfg.py
@@ -2,10 +2,28 @@ import FWCore.ParameterSet.Config as cms
 
 readFiles = cms.untracked.vstring()
 secFiles = cms.untracked.vstring() 
-source = cms.Source ("PoolSource",fileNames = readFiles, secondaryFileNames = secFiles)
+source = cms.Source ("PoolSource",
+                     fileNames = readFiles, 
+                     secondaryFileNames = secFiles,
+#                     skipEvents = cms.untracked.uint32(324),
+)
 readFiles.extend( [  
 #'file:/home/mangano/WW/TTbarJets_-START3X_V26_S09.root',
-'file:/data/mangano/MC/WW/WW_START3X_V26_S09.root',
+
+"file:/data/gpetrucc/7TeV/ww/store/user/gpetrucc/WW/GenLevelSkim_Spring10-START3X_V26_S09-v1/2d32bcc4f128a3b5966c4cd81a8a2e76/test_10_1_u74.root",
+"file:/data/gpetrucc/7TeV/ww/store/user/gpetrucc/WW/GenLevelSkim_Spring10-START3X_V26_S09-v1/2d32bcc4f128a3b5966c4cd81a8a2e76/test_11_1_lXF.root",
+"file:/data/gpetrucc/7TeV/ww/store/user/gpetrucc/WW/GenLevelSkim_Spring10-START3X_V26_S09-v1/2d32bcc4f128a3b5966c4cd81a8a2e76/test_12_1_ozc.root",
+"file:/data/gpetrucc/7TeV/ww/store/user/gpetrucc/WW/GenLevelSkim_Spring10-START3X_V26_S09-v1/2d32bcc4f128a3b5966c4cd81a8a2e76/test_13_1_Pmh.root",
+"file:/data/gpetrucc/7TeV/ww/store/user/gpetrucc/WW/GenLevelSkim_Spring10-START3X_V26_S09-v1/2d32bcc4f128a3b5966c4cd81a8a2e76/test_1_1_u0x.root",
+"file:/data/gpetrucc/7TeV/ww/store/user/gpetrucc/WW/GenLevelSkim_Spring10-START3X_V26_S09-v1/2d32bcc4f128a3b5966c4cd81a8a2e76/test_2_1_KQI.root",
+"file:/data/gpetrucc/7TeV/ww/store/user/gpetrucc/WW/GenLevelSkim_Spring10-START3X_V26_S09-v1/2d32bcc4f128a3b5966c4cd81a8a2e76/test_3_1_fTy.root",
+"file:/data/gpetrucc/7TeV/ww/store/user/gpetrucc/WW/GenLevelSkim_Spring10-START3X_V26_S09-v1/2d32bcc4f128a3b5966c4cd81a8a2e76/test_4_1_XjO.root",
+"file:/data/gpetrucc/7TeV/ww/store/user/gpetrucc/WW/GenLevelSkim_Spring10-START3X_V26_S09-v1/2d32bcc4f128a3b5966c4cd81a8a2e76/test_5_1_j8A.root",
+"file:/data/gpetrucc/7TeV/ww/store/user/gpetrucc/WW/GenLevelSkim_Spring10-START3X_V26_S09-v1/2d32bcc4f128a3b5966c4cd81a8a2e76/test_6_1_xKg.root",
+"file:/data/gpetrucc/7TeV/ww/store/user/gpetrucc/WW/GenLevelSkim_Spring10-START3X_V26_S09-v1/2d32bcc4f128a3b5966c4cd81a8a2e76/test_7_1_IKi.root",
+"file:/data/gpetrucc/7TeV/ww/store/user/gpetrucc/WW/GenLevelSkim_Spring10-START3X_V26_S09-v1/2d32bcc4f128a3b5966c4cd81a8a2e76/test_8_1_33L.root",
+"file:/data/gpetrucc/7TeV/ww/store/user/gpetrucc/WW/GenLevelSkim_Spring10-START3X_V26_S09-v1/2d32bcc4f128a3b5966c4cd81a8a2e76/test_9_1_Rxw.root"
+#'file:/data/mangano/MC/WW/WW_START3X_V26_S09.root',
 ]);
 
 
@@ -33,6 +51,7 @@ process.GlobalTag.globaltag = 'START36_V9::All'
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 process.source = source
+
 
 #======================================
 # HLT
@@ -65,6 +84,9 @@ process.load('WWAnalysis.Filters.softMuonVeto_cfi')
 
 process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) ) 
 
+#process.Tracer = cms.Service("Tracer")
+#process.contentAna = cms.EDAnalyzer("EventContentAnalyzer")
+
 process.p2 = cms.Path(process.selectionHLT*
                       process.elePlusSelection1*process.eleMinusSelection1*     #sel1  
                       process.elePlusIPSelection*process.eleMinusIPSelection*   #sel2
@@ -90,6 +112,6 @@ process.out = cms.OutputModule("PoolOutputModule",
                                            'keep *_*_*_WWSel'),
     SelectEvents = cms.untracked.PSet(SelectEvents = cms.vstring("p2"))
 )
-process.end = cms.EndPath(process.out)
+#process.end = cms.EndPath(process.out)
 
 
