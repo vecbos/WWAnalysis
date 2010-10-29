e83c5605e86ebd86dcba2d5714e826ebdc04eee7
mangano <>
2010-10-29 13:29:42 +0000

    update to new skim label

--- AnalysisStep/scripts/eventProd.sh
+++ AnalysisStep/scripts/eventProd.sh
@@ -11,95 +11,95 @@ action=$1
 
 DIR="${CMSSW_BASE}/src/WWAnalysis/AnalysisStep"
 
-label=oct21
+label=oct28
 dataset=boh
 
 case $2 in
     ### Wjets:
     1)
 	dataset=/Wmunu/mangano-SkimStep005-bkg1-oct15-2455c73529abf8a64e80894749518608/USER
-	taskName=EventProd006-bkg1-$label
+	taskName=EventProd008-bkg1-$label
 	;;
     2)
 	dataset=/Wenu/mangano-SkimStep005-bkg2-oct15-2455c73529abf8a64e80894749518608/USER
-	taskName=EventProd006-bkg2-$label
+	taskName=EventProd008-bkg2-$label
 	;;
     3)
 	dataset=/Wtaunu/mangano-SkimStep005-bkg3-oct15-2455c73529abf8a64e80894749518608/USER
-	taskName=EventProd006-bkg3-$label
+	taskName=EventProd008-bkg3-$label
 	;;
     4)
 	dataset=/Zgamma_2l_enriched/mangano-SkimStep005-bkg4-v9-2455c73529abf8a64e80894749518608/USER 
-	taskName=EventProd006-bkg4-$label
+	taskName=EventProd008-bkg4-$label
 	;;
     5)
 	dataset=/WW_2l_7TeV/mangano-SkimStep005-bkg5-v9-2455c73529abf8a64e80894749518608/USER
-	taskName=EventProd006-bkg5-$label
+	taskName=EventProd008-bkg5-$label
 	;;
     6)
 	dataset=/gg_WW_2l2nu-GG2WW/mangano-SkimStep005-bkg6-oct15-2455c73529abf8a64e80894749518608/USER 
-	taskName=EventProd006-bkg6-$label
+	taskName=EventProd008-bkg6-$label
 	;;
     7)
 	dataset=/WZ/mangano-SkimStep005-bkg7-oct15-2455c73529abf8a64e80894749518608/USER 
-	taskName=EventProd006-bkg7-$label
+	taskName=EventProd008-bkg7-$label
 	;;
     8)
 	dataset=/ZZ/mangano-SkimStep005-bkg8-oct15-2455c73529abf8a64e80894749518608/USER 
-	taskName=EventProd006-bkg8-$label
+	taskName=EventProd008-bkg8-$label
 	;;
 #    9)
 #	dataset=Wgammma
-#	taskName=EventProd006-bkg9-$label
+#	taskName=EventProd008-bkg9-$label
 #	;;
 #    10)
 #	dataset=Zgamma
-#	taskName=EventProd006-bkg10-$label
+#	taskName=EventProd008-bkg10-$label
 #	;;
     11)
 	dataset=/TTbar_2l/mangano-SkimStep005-bkg11-oct15-2455c73529abf8a64e80894749518608/USER 
-	taskName=EventProd006-bkg11-$label
+	taskName=EventProd008-bkg11-$label
 	;;
     12)
 	dataset=/SingleTop_tChannel-madgraph/mangano-SkimStep005-bkg12-oct15-2455c73529abf8a64e80894749518608/USER 
-	taskName=EventProd006-bkg12-$label
+	taskName=EventProd008-bkg12-$label
 	;;
     13)
 	dataset=/SingleTop_sChannel-madgraph/mangano-SkimStep005-bkg13-oct15-2455c73529abf8a64e80894749518608/USER 
-	taskName=EventProd006-bkg13-$label
+	taskName=EventProd008-bkg13-$label
 	;;
     14)
 	dataset=/SingleTop_tWChannel-madgraph/mangano-SkimStep005-bkg14-oct15-2455c73529abf8a64e80894749518608/USER 
-	taskName=EventProd006-bkg14-$label
+	taskName=EventProd008-bkg14-$label
 	;;
     15)
 	dataset=/WW/mangano-SkimStep005-bkg15-oct15-2455c73529abf8a64e80894749518608/USER 
-	taskName=EventProd006-bkg15-$label
+	taskName=EventProd008-bkg15-$label
 	;;
     16)
 	dataset=/WJets-madgraph/mangano-SkimStep005-bkg16-oct15-2455c73529abf8a64e80894749518608/USER 
-	taskName=EventProd006-bkg16-$label
+	taskName=EventProd008-bkg16-$label
 	;;
     17)
 	dataset=/Zee/mangano-SkimStep005-bkg17-oct15-2455c73529abf8a64e80894749518608/USER 
-	taskName=EventProd006-bkg17-$label
+	taskName=EventProd008-bkg17-$label
 	;;
     18)
 	dataset=/Zmumu/mangano-SkimStep005-bkg18-oct15-2455c73529abf8a64e80894749518608/USER 
-	taskName=EventProd006-bkg18-$label
+	taskName=EventProd008-bkg18-$label
 	;;
     19)
 	dataset=/Ztautau/mangano-SkimStep005-bkg19-oct15-2455c73529abf8a64e80894749518608/USER 
-	taskName=EventProd006-bkg19-$label
+	taskName=EventProd008-bkg19-$label
 	;;
     20)
 	dataset=/TTbarJets-madgraph/mangano-SkimStep005-bkg20-oct15-2455c73529abf8a64e80894749518608/USER 
-	taskName=EventProd006-bkg20-$label
+	taskName=EventProd008-bkg20-$label
 	;;
     ### signals
     115)
 	dataset=/H160_2W_2lnu_gluonfusion_7TeV/mangano-SkimStep005-sig115-oct15-2455c73529abf8a64e80894749518608/USER
-	taskName=EventProd006-sig115-$label
+	taskName=EventProd008-sig115-$label
 	;;
 
 
