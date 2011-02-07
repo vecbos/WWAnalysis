d66651c7fd737e27ce68746033a3e3a1c93fb45c
mangano <>
2011-02-07 16:06:24 +0000

    added IP stuff

--- AnalysisStep/src/PatMuonBooster.cc
+++ AnalysisStep/src/PatMuonBooster.cc
@@ -20,6 +20,14 @@
 #include "TrackingTools/Records/interface/TransientTrackRecord.h"
 
 #include "RecoVertex/GaussianSumVertexFit/interface/GsfVertexTrackCompatibilityEstimator.h"
+#include "WWAnalysis/Tools/interface/VertexReProducer.h"
+#include "CommonTools/UtilAlgos/interface/MatchByDRDPt.h"
+
+
+//for sim-reco IP bias
+#include "SimTracker/Records/interface/TrackAssociatorRecord.h"
+#include "SimTracker/TrackAssociation/plugins/ParametersDefinerForTPESProducer.h"
+
 
 //
 // class declaration
@@ -80,8 +88,19 @@ PatMuonBooster::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
     using namespace edm;
     using namespace std;
+
+    ParameterSet psetMatcher;
+    psetMatcher.addParameter<double>("maxDPtRel",0.30);
+    psetMatcher.addParameter<double>("maxDeltaR",0.010);
+
+    reco::MatchByDRDPt<reco::Track,reco::Track> ttMatcher(psetMatcher);
+    reco::MatchByDRDPt<reco::Track,reco::GsfTrack> tgMatcher(psetMatcher);
+
     ESHandle<TransientTrackBuilder> theTTBuilder;
     iSetup.get<TransientTrackRecord>().get("TransientTrackBuilder",theTTBuilder);
+
+    ESHandle<ParametersDefinerForTP> parametersDefinerTP; 
+    iSetup.get<TrackAssociatorRecord>().get("LhcParametersDefinerForTP",parametersDefinerTP); 
 
 
     Handle<pat::MuonCollection> muons;
@@ -90,54 +109,145 @@ PatMuonBooster::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
     Handle<reco::VertexCollection> vertices;
     iEvent.getByLabel(vertexTag_,vertices);
 
+    Handle<reco::BeamSpot> bs;
+    iEvent.getByLabel(InputTag("offlineBeamSpot"),bs);
+
+    VertexReProducer revertex(vertices, iEvent);
+    Handle<reco::TrackCollection> pvtracks;   iEvent.getByLabel(revertex.inputTracks(),   pvtracks);
+    Handle<reco::BeamSpot>        pvbeamspot; iEvent.getByLabel(revertex.inputBeamSpot(), pvbeamspot);
+
     //   if(vertices->size()==0) return;
     //   reco::Vertex vertex = vertices->front();
 
     reco::Vertex vertex;
-    if(vertices->size()==0) {
-//         vertex.SetXYZ(0,0,0);
-        vertex = reco::Vertex(reco::Vertex::Point(0,0,0), reco::Vertex::Error());
-    } else {
-        vertex = vertices->front();
-    }
+    reco::Vertex vertex2;
 
-
-
-    //Use the ExampleData to create an ExampleData2 which 
-    // is put into the Event
     std::auto_ptr<pat::MuonCollection> pOut(new pat::MuonCollection);
     for(pat::MuonCollection::const_iterator mu=muons->begin(); mu!=muons->end(); ++mu){
         pat::Muon clone = *mu;
-        if(mu->type() != 8){
-            //        reco::TransientTrack tt = theTTBuilder->build(mu->track());
-            //        Measurement1D ip = IPTools::absoluteTransverseImpactParameter(tt,vertex).second;
-            //        Measurement1D ip3D = IPTools::absoluteImpactParameter3D(tt,vertex).second;
+        if(mu->type() == 8) continue;
 
-            //        clone.addUserFloat(std::string("tip"),ip.value());
-            //        clone.addUserFloat(std::string("tipErr"),ip.error());
-            //        clone.addUserFloat(std::string("ip"),ip3D.value());
-            //        clone.addUserFloat(std::string("ipErr"),ip3D.error());
+	// -------- here is the comparison with the TP
+	/*
+	TrackingParticle tp;
+	tp.setP4(mu->genParticleRef()->p4());
+	tp.setVertex(mu->genParticleRef()->vertex(),0.);
+	tp.setThreeCharge(mu->genParticleRef()->charge()*3.0);
+
+	ParticleBase::Vector momentumTP = parametersDefinerTP->momentum(iEvent,iSetup,tp);
+	ParticleBase::Point vertexTP = parametersDefinerTP->vertex(iEvent,iSetup,tp);
+	double chargeTP = tp.charge();
+	
+	double qoverpSim = chargeTP/sqrt(momentumTP.x()*momentumTP.x()+momentumTP.y()*momentumTP.y()+momentumTP.z()*momentumTP.z());
+	double lambdaSim = M_PI/2-momentumTP.theta();
+	double phiSim    = momentumTP.phi();
+	double dxySim    = (-vertexTP.x()*sin(momentumTP.phi())+vertexTP.y()*cos(momentumTP.phi()));
+	double dzSim     = vertexTP.z() - (vertexTP.x()*momentumTP.x()+vertexTP.y()*momentumTP.y())/sqrt(momentumTP.perp2()) 
+	  * momentumTP.z()/sqrt(momentumTP.perp2());
+
+	//double dxy = mu->gsfTrack()->dxy(vertices->front().position());
+	double dxy = mu->track()->dxy(bs->position());
+	double dz = mu->track()->dz(bs->position());
+
+	double dxyPull = (dxy-dxySim)/mu->track()->dxyError(); 
+	double dzPull = (dz-dzSim)/mu->track()->dzError(); 
+	
+	clone.addUserFloat(std::string("dxy"),dxy-dxySim);
+	clone.addUserFloat(std::string("dz"),dz-dzSim);
+	clone.addUserFloat(std::string("dxyPull"),dxyPull);
+	clone.addUserFloat(std::string("dzPull"),dzPull);
+	*/
+
+      // ------- here I add the information about the IP significance wrt the PV
+	reco::TrackCollection newTkCollection;
+	bool foundMatch(false);
+	for(reco::TrackCollection::const_iterator itk = pvtracks->begin(); itk != pvtracks->end(); ++itk){    
+	  bool refMatching= (&*itk == &*(mu->reco::Muon::innerTrack()) ); 
+	  if(refMatching){
+	    //if(ttMatcher(*itk,*(mu->track())) && itk->charge()==mu->charge()){	   
+	    newTkCollection.assign(pvtracks->begin(), itk);
+	    newTkCollection.insert(newTkCollection.end(),itk+1,pvtracks->end());
+	    foundMatch = true;
+	    break;
+	  }
+	}
+
+	if(!foundMatch) {
+	  cout << "WARNING: matching to muon was not found" << endl;
+	  if(vertices->empty()) {
+	    vertex = reco::Vertex(reco::Vertex::Point(bs->position().x(),bs->position().y(),bs->position().z()),
+				  reco::Vertex::Error());
+	  } else {
+	    vertex = vertices->front();
+	  }
+	}else{      
+	  vector<TransientVertex> pvs = revertex.makeVertices(newTkCollection, *pvbeamspot, iSetup) ;
+	  if(pvs.empty()) {
+	    vertex = reco::Vertex(reco::Vertex::Point(bs->position().x(),bs->position().y(),bs->position().z()),
+				  reco::Vertex::Error());
+	  } else {
+	    vertex = reco::Vertex(pvs.front());
+	  }
+	}
 
 
-            //        try{
-            // 	 GsfVertexTrackCompatibilityEstimator vertexTrackCompEst;       
-            // 	 GsfVertexTrackCompatibilityEstimator::BDpair result = vertexTrackCompEst.estimate(vertex,tt);
-            // 	 if(result.first)
-            // 	   clone.addUserFloat(std::string("vtxComp"),result.second);
-            // 	 else
-            // 	   clone.addUserFloat(std::string("vtxComp"),99999.);
-            //        }catch(...){
-            // 	 cout << "exception caught" << endl;
-            // 	 clone.addUserFloat(std::string("vtxComp"),99999.);
-            //        }
+	if(vertices->empty()) {
+	  vertex2 = reco::Vertex(reco::Vertex::Point(bs->position().x(),bs->position().y(),bs->position().z()),
+				 reco::Vertex::Error());
+	} else {
+	  vertex2 = vertices->front();
+	}
 
-            clone.addUserFloat( "dxyPV",clone.track()->dxy(vertices->front().position()) );
-            clone.addUserFloat( "dzPV",clone.track()->dz(vertices->front().position()) );
+	reco::TransientTrack tt = theTTBuilder->build(mu->track());
+	Measurement1D ip = IPTools::absoluteTransverseImpactParameter(tt,vertex).second;
+	Measurement1D ip3D = IPTools::absoluteImpactParameter3D(tt,vertex).second;
+	Measurement1D ip_2 = IPTools::absoluteTransverseImpactParameter(tt,vertex2).second;
+	Measurement1D ip3D_2 = IPTools::absoluteImpactParameter3D(tt,vertex2).second;
 
-        } else {
-            clone.addUserFloat( "dxyPV", 9999);
-            clone.addUserFloat( "dzPV", 9999);
-        }
+	clone.addUserFloat(std::string("tip"),ip.value());
+	clone.addUserFloat(std::string("tipErr"),ip.error());
+	clone.addUserFloat(std::string("ip"),ip3D.value());
+	clone.addUserFloat(std::string("ipErr"),ip3D.error());
+
+	clone.addUserFloat(std::string("tip2"),ip_2.value());
+	clone.addUserFloat(std::string("tipErr2"),ip_2.error());
+	clone.addUserFloat(std::string("ip2"),ip3D_2.value());
+	clone.addUserFloat(std::string("ipErr2"),ip3D_2.error());
+
+
+      // ------- here I add the information about the electron-PV compatibility
+	try{
+	  GsfVertexTrackCompatibilityEstimator vertexTrackCompEst;       
+	  GsfVertexTrackCompatibilityEstimator::BDpair result = vertexTrackCompEst.estimate(vertex,tt);
+	  if(result.first)
+	    clone.addUserFloat(std::string("vtxComp"),result.second);
+	  else
+	    clone.addUserFloat(std::string("vtxComp"),99999.);
+	}catch(...){
+	  cout << "WARNING, failed to evaluate ele-PV compatibility in PatMuonBooster" << endl;
+	  clone.addUserFloat(std::string("vtxComp"),99999.);
+	}
+
+
+	try{
+	  GsfVertexTrackCompatibilityEstimator vertexTrackCompEst;       
+	  GsfVertexTrackCompatibilityEstimator::BDpair result = vertexTrackCompEst.estimate(vertex2,tt);
+	  if(result.first)
+	    clone.addUserFloat(std::string("vtxComp2"),result.second);
+	  else
+	    clone.addUserFloat(std::string("vtxComp2"),99999.);
+	  //cout << "here4" << endl;
+	}catch(...){
+	  cout << "WARNING, failed to evaluate ele-PV compatibility in PatElectronBooster" << endl;
+	  clone.addUserFloat(std::string("vtxComp2"),99999.);
+	}
+
+	// ------- here I add the information about the absolute IP  wrt the PV
+	clone.addUserFloat( "dxyPV",clone.track()->dxy(vertices->front().position()) );
+	clone.addUserFloat( "dzPV",clone.track()->dz(vertices->front().position()) );
+	//clone.addUserFloat( "pOut",clone.track()->outerP() );
+
+
         pOut->push_back(clone);
     }
 
