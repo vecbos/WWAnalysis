4f6fc538cdcde07dfe26413b374a105540347f01
mangano <>
2011-04-25 11:25:09 +0000

    add edmWrapper for the generic analyzer

--- AnalysisStep/test/fwLiteBorisSimpleYielder_cfg.py
+++ AnalysisStep/test/fwLiteBorisSimpleYielder_cfg.py
@@ -3,13 +3,15 @@ import FWCore.ParameterSet.Config as cms
 process = cms.PSet()
 
 process.fwliteInput = cms.PSet(
-    fileNames   = cms.vstring('file:/data/mangano/MC/Spring11/WW_414_SKIM_V04/ggToH150toWWto2L2Nu.step2.root'), ## mandatory
+    fileNames   = cms.vstring(
+        'file:/data/mangano/MC/Spring11/WW_414_SKIM_V04/ggToH150toWWto2L2Nu.step2.root'
+        ), ## mandatory
     maxEvents   = cms.int32(-1),                            ## optional
     outputEvery = cms.uint32(100),                            ## optional
 )
     
 process.fwliteOutput = cms.PSet(
-    fileName  = cms.string('histo.fw.root'),  ## mandatory
+    fileName  = cms.string('histo.fwlite.root'),  ## mandatory
 )
 
 process.myAnalyzer = cms.PSet(
