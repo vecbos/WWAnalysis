ccc30a711fdd6ed8ce22a971ac83cd0d8e973338
mwlebour <>
2011-05-07 20:13:10 +0000

    added the script from jonatan

--- AnalysisStep/scripts/newCombinePlots.py
+++ AnalysisStep/scripts/newCombinePlots.py
@@ -1,17 +1,22 @@
-import sys
-import math 
-import re
+import sys, os, math, re
 
 from ROOT import gROOT, gStyle, TCanvas, TH1F, TFile, TDirectoryFile, TStyle, TLegend, THStack
 from ROOT import kCyan, kGreen, kMagenta, kBlue, kWhite, kOrange, kViolet, kBlack
-from ROOT import TObject 
+from ROOT import TObject, gInterpreter, Long
 from WWAnalysis.AnalysisStep.scaleFactors_cff import *
 from WWAnalysis.AnalysisStep.yieldProducer_cfi import FWLiteParams
+
+gROOT.LoadMacro( os.getenv("CMSSW_BASE")+"/src/WWAnalysis/Misc/scripts/LatinoPlot.C+" )
+gInterpreter.ExecuteMacro( os.getenv("CMSSW_BASE")+"/src/WWAnalysis/Misc/scripts/LatinoStyle.C" )
+from ROOT import LatinoPlot
+
+# from ROOT import LatinoPlot
 
 # loopables and other default that can probably be configured later
 channels = ['wwelel','wwelmu','wwmuel','wwmumu']
 suffixes = ['IPLHT']
 labels  =                 [       'HWW',      'WW',  'Z+Jets',        'Top',        'WW/WZ',    'W+Jets', ]
+myEnum  = dict(zip(labels,[           0,         1,         2,            3,              4,           5, ]))
 colors  = dict(zip(labels,[          10, kOrange-9, kViolet-9,   kMagenta-9,       kGreen-9,     kCyan-9, ]))
 samples = dict(zip(labels,[ h160Samples, wwSamples, dySamples, ttbarSamples, diBosonSamples, wJetSamples, ]))
 plotInfos = FWLiteParams.histParams.clone()
@@ -32,30 +37,30 @@ gStyle.SetOptStat(0)
 tempHist = {}
 
 
-def setUpLegend(colors,labels):
-    legend = {}
-    legend[0] = TLegend(0.2,0.8,0.4,0.9)
-    legend[1] = TLegend(0.45,0.8,0.65,0.9)
-    legend[2] = TLegend(0.7,0.8,0.9,0.9)
-    legend[0].SetFillColor(0)
-    legend[0].SetLineColor(0)
-    legend[0].SetLineWidth(0)
-    legend[1].SetFillColor(0)
-    legend[1].SetLineColor(0)
-    legend[1].SetLineWidth(0)
-    legend[2].SetFillColor(0)
-    legend[2].SetLineColor(0)
-    legend[2].SetLineWidth(0)
-    
-    i = 0
-    for label in labels:
-        tempHist[i] = TH1F("asdf"+str(i),"asdf",2,0,2)
-        tempHist[i].SetLineColor(kBlack)
-        tempHist[i].SetLineWidth(2)
-        tempHist[i].SetFillColor(colors[label]);
-        legend[i/2].AddEntry(tempHist[i],label,"f")
-        i = i+1
-    return legend
+# def setUpLegend(colors,labels):
+#     legend = {}
+#     legend[0] = TLegend(0.2,0.8,0.4,0.9)
+#     legend[1] = TLegend(0.45,0.8,0.65,0.9)
+#     legend[2] = TLegend(0.7,0.8,0.9,0.9)
+#     legend[0].SetFillColor(0)
+#     legend[0].SetLineColor(0)
+#     legend[0].SetLineWidth(0)
+#     legend[1].SetFillColor(0)
+#     legend[1].SetLineColor(0)
+#     legend[1].SetLineWidth(0)
+#     legend[2].SetFillColor(0)
+#     legend[2].SetLineColor(0)
+#     legend[2].SetLineWidth(0)
+#     
+#     i = 0
+#     for label in labels:
+#         tempHist[i] = TH1F("asdf"+str(i),"asdf",2,0,2)
+#         tempHist[i].SetLineColor(kBlack)
+#         tempHist[i].SetLineWidth(2)
+#         tempHist[i].SetFillColor(colors[label]);
+#         legend[i/2].AddEntry(tempHist[i],label,"f")
+#         i = i+1
+#     return legend
 
 
 def getStackedPlot(plotPath):
@@ -74,13 +79,19 @@ def getStackedPlot(plotPath):
                 addablePlots[label] = thisPlot.Clone(label+"stack")
                 addablePlots[label].Scale(lumi*samples[label][key][1])
     
-    hs = THStack("hs","hs")
+    hs = LatinoPlot()
+    hs.setLumi(lumi*1000)
+    re1='(.*?)' # Non-greedy match on filler
+    re2='(\\[)' # Any Single Character 1
+    re3='(.*?)' # Word 1
+    re4='(\\])' # Any Single Character 2
+    rg = re.compile(re1+re2+re3+re4,re.IGNORECASE|re.DOTALL)
     for label in labels:
-        addablePlots[label].SetLineColor(kBlack);
-        addablePlots[label].SetLineWidth(2);
-        addablePlots[label].SetFillColor(colors[label]);
-        addablePlots[label].Rebin(rebinBy)
-        hs.Add(addablePlots[label])
+        hs.setMCHist(Long(myEnum[label]),addablePlots[label])
+        # I hate myself for this ...
+        m = rg.search(addablePlots[label].GetXaxis().GetTitle())
+        hs.setLabel(m.group(1))
+        hs.setUnits(m.group(3))
     return hs
     
 def getDataPlot(plotPath):
@@ -128,56 +139,12 @@ def getYLabel(currentPlot):
 #            |___/                                
 
 
-#Setup the legend
-legend = setUpLegend(colors,labels)
 
-currentPlot = "mll"
-currentCut = "06"
+currentPlot = "dPhill"
+currentCut = "12"
 plotPath = "eventHists/bycut/"+channels[0]+suffixes[0]+"/"+currentPlot+"/"+currentCut+"/"
 
 # Can loop here, but just using defaults from above for now
 hs = getStackedPlot(plotPath)
-
-hs.Draw("HIST")
-hs.GetXaxis().SetTitle( getattr(plotInfos,currentPlot).xtitle.value() )
-hs.GetYaxis().SetTitle( getYLabel(currentPlot) )
-
-#Add the data 
-# data = getDataPlot(plotPath)
-# data.Draw("SAME")
-
-#Draw the legend
-for l in legend: legend[l].Draw()
-
-#Add the data 
-
-#Add a line for preliminary?
-
-# Make room for the stuff at the top
-logMax = 1000.0 * hs.GetMaximum()
-linMax = 2.0    * hs.GetMaximum()
-  
-hs.SetMaximum(linMax)
+hs.Draw(10)
 c1.Print("lin.png")
-# for fileType in fileTypes:
-#     dir = plotPath[:-1].replace("eventHists","plots/"+fileType)+".lin."+fileType)
-#     if not os.path.exists(dir):
-#         os.makedirs(dir)
-#     c1.Print(
-  
-c1.SetLogy()
-hs.SetMaximum(logMax)
-hs.SetMinimum(logMax/2e5)
-c1.Print("log.png")
-
-
-
-
-
-
-
-
-
-
-
-
