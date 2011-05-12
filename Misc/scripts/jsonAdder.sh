6fa8f5cd9dd41911415efd9271a607017403c4a5
mwlebour <>
2011-05-12 09:39:56 +0000

    146095898.630 in certified ucsd

--- Misc/scripts/jsonAdder.sh
+++ Misc/scripts/jsonAdder.sh
@@ -16,7 +16,7 @@ compareJSON.py --and output3.json                     output4.json              
 compareJSON.py --or  output5.json                     output6.json                           Jsons/ucsdCrabOutput.json
 
 # get the certified UCSD json
-compareJSON.py --or  Jsons/ucsdCrabOutput.json        Jsons/certified2011.json               Jsons/certifiedUCSD.json
+compareJSON.py --and  Jsons/ucsdCrabOutput.json       Jsons/certified2011.json               Jsons/certifiedUCSD.json
 
 # then take the intersection with ema's json
 compareJSON.py --and Jsons/ucsdCrabOutput.json        Jsons/romeCrabOutput.json              combinedCrabOutput.json
