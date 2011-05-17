5bb50517c84fc343c8a27582d2cc00ecb94abacf
gpetrucc <>
2011-05-17 16:25:33 +0000

    Add dataset id, set flobal scale factor, allow choosing JSON

--- AnalysisStep/test/step3/step3.py
+++ AnalysisStep/test/step3/step3.py
@@ -1,4 +1,5 @@
 import FWCore.ParameterSet.Config as cms
+import re
 import sys
 args = sys.argv[1:]
 if (sys.argv[0] == "cmsRun"): args =sys.argv[2:]
@@ -18,25 +19,32 @@ process.source.inputCommands = cms.untracked.vstring( "keep *", "drop *_conditio
 process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(True))
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
-dataset = ['MC','ggH', 160];
-if len(args) > 0:
-    if args[0] in [ 'SingleElectron', 'DoubleElectron', 'SingleMuon', 'DoubleMuon', 'MuEG']:
-        dataset = [ args[0] ]
-    elif 'ggH' in args[0]:
-        dataset = ['MC','ggH', int(args[0].replace('ggH','')) ]
-    else:
-        dataset = ['MC', args[0]]
+if len(args) == 0: args = [ 'ggToH160toWWto2L2Nu', 101160, 0.003621062529384]
+if len(args) != 3: raise RuntimeError, "step3.py dataset id json (for data) or step3.py dataset id scalefactor (for MC)"
+## step3.py dataset id json   for data
+## step3.py dataset id scalef for MC
+dataset = ['MC','ggToH160toWWto2L2Nu']; id = 101160; 
+scalef  = 0.003621062529384;
+json    = None
+mhiggs  = 0
+args[0] = args[0].replace("2011Av2","").replace("2011A","")
+if args[0] in [ 'SingleElectron', 'DoubleElectron', 'SingleMuon', 'DoubleMuon', 'MuEG']:
+    dataset = [args[0]]; id = args[1]
+    json    = args[2]
+else:
+    dataset = ['MC', args[0]]; id = args[1];
+    scalef  = float(args[2])
+    m = re.match("ggToH(\\d+)to.*", args[0])
+    if m: mhiggs = int(m.group(1))
 
 nverticesModule = cms.EDProducer("VertexMultiplicityCounter",
     probes = cms.InputTag("REPLACE_ME"),
     objects = cms.InputTag("offlinePrimaryVertices"),
     objectSelection = cms.string("!isFake && ndof > 4 && abs(z) <= 25 && position.Rho <= 2"),
 )
-
 Template = cms.EDFilter("ProbeTreeProducer",
     cut = cms.string("q(0)*q(1) < 0 && !isSTA(0) && !isSTA(1) && "+
                      "leptEtaCut(2.4,2.5) && "+
-                     #"ptMin() > 10 && ptMax() > 20 && "+
                      "ptMax > 20 && "+
                      "((abs(pdgIdByPt(1)) == 13 && ptMin > 10) || "+
                      " (abs(pdgIdByPt(1)) == 11 && ptMin > 15)) && "
@@ -73,6 +81,7 @@ Template = cms.EDFilter("ProbeTreeProducer",
         bestiso  = cms.string("allIsoByIso(0)/ptByIso(0)"),
         worstiso = cms.string("allIsoByIso(1)/ptByIso(1)"),
         worstJetLepPt = cms.string("max(matchedJetPt(0, 0.5)/pt(0), matchedJetPt(1, 0.5)/pt(1))"),
+        dataset = cms.string(str(id))
     ),
     flags = cms.PSet(
         sameflav = cms.string("hypo == 3 || hypo == 6"),
@@ -87,14 +96,15 @@ if dataset[0] == "MC":
     from WWAnalysis.AnalysisStep.pileupReweighting_cfi import reWeightVector
     Template.eventWeight = cms.InputTag("mcWeight");
     process.mcWeight     = cms.EDProducer("CombinedWeightProducer",
-        puWeight = cms.vdouble(*reWeightVector[:]),
-        puLabel  = cms.InputTag("addPileupInfo"),
+        baseWeight = cms.double(scalef),
+        puWeight   = cms.vdouble(*reWeightVector[:]),
+        puLabel    = cms.InputTag("addPileupInfo"),
     )
-    if dataset[1] == "ggH":
+    if mhiggs > 0:
         process.higgsPt = cms.EDProducer("HWWKFactorProducer",
-            genParticles = cms.untracked.InputTag("onlyHiggsGen"),
-            inputFilename = cms.untracked.string("WWAnalysis/Misc/Scales/scalefactor.mh%d.dat" % dataset[2]),
-            ProcessID = cms.untracked.int32(0), #10010), # 0: don't check
+            genParticlesTag = cms.InputTag("onlyHiggsGen"),
+            inputFilename = cms.untracked.string("WWAnalysis/Misc/Scales/scalefactor.mh%d.dat" % mhiggs),
+            ProcessID = cms.untracked.int32(10010),
             Debug =cms.untracked.bool(False)
         )
         process.mcWeight.ptWeight = cms.InputTag("higgsPt")
@@ -104,7 +114,7 @@ if dataset[0] == "MC":
 else:
     from FWCore.PythonUtilities.LumiList import LumiList
     import os    
-    lumis = LumiList(filename = os.getenv('CMSSW_BASE')+'/src/WWAnalysis/Misc/Jsons/certifiedUCSD.json')
+    lumis = LumiList(filename = os.getenv('CMSSW_BASE')+'/src/WWAnalysis/Misc/Jsons/%s.json'%json)
     process.source.lumisToProcess = cms.untracked.VLuminosityBlockRange()
     process.source.lumisToProcess = lumis.getCMSSWString().split(',')
 
