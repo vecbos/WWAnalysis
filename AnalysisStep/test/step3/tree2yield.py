c45fed5ccc7383ec45bde01cd3dfca85dce74ba7
gpetrucc <>
2011-05-17 16:27:03 +0000

    Some pieces, although not yet the full picture

--- AnalysisStep/test/step3/tree2yield.py
+++ AnalysisStep/test/step3/tree2yield.py
@@ -12,11 +12,18 @@ class CutsFile:
             self._cuts.append((name.strip(),cut.strip()))
     def cuts(self):
         return self._cuts[:]
+    def nMinusOneCuts(self):
+        ret = []
+        for c in self._cuts:
+            nm1 = " && ".join("(%s)" % x[1] for x in self._cuts if x[0] != c[0])
+            ret.append(("all but "+name, nm1))
+        return ret
     def allCuts(self):
         return " && ".join("(%s)" % x[1] for x in self._cuts)
 
 class TreeToYield:
     def __init__(self,root,options,report=None):
+        self._fname = root
         self._tfile = ROOT.TFile.Open(root)
         if not self._tfile: raise RuntimeError, "Cannot open %s\n" % root
         self._options = options
@@ -25,6 +32,16 @@ class TreeToYield:
             t = self._tfile.Get((options.tree % h)+"/probe_tree")
             if not t: raise RuntimeError, "Cannot find tree %s/probe_tree in file %s\n" % (options.tree % h, root)
             self._trees.append((h,t))
+    def attachMVA(self,name):
+        self._fnameMVA = self._fname.replace(".root","."+name+".root")
+        self._tfileMVA = ROOT.TFile.Open(self._fnameMVA)
+        if not self._tfileMVA: raise RuntimeError, "Cannot open %s\n" % self._fnameMVA
+        self._treesMVA = []
+        for h,t0 in self._trees:
+            t = self._tfile.Get((options.tree % h)+"/"+name)
+            if not t: raise RuntimeError, "Cannot find tree %s/%s in file %s\n" % (options.tree % h, name, self._fnameMVA)
+            self._treesMVA.append((h,t))
+            t0.AddFriend(t)
     def getYields(self,cuts):
         report = []; cut = ""
         for cn,cv in cuts:
@@ -45,8 +62,13 @@ class TreeToYield:
             for j,(hypo,nev) in enumerate(yields):
                 den = report[i-1][1][j][1] if i>0 else 0
                 fraction = nev/float(den) if den > 0 else 1
-                print "%7d  %6.2f%%   " % (nev, fraction * 100),
+                if self._options.weight:
+                    print "%7.2f  %6.2f%%   " % (nev, fraction * 100),
+                else:
+                    print "%7d  %6.2f%%   " % (nev, fraction * 100),
             print ""
+    def getPlots(self,expr,name,bins,cut):
+        return [ (k,self._getPlot(t,expr,name,bins,cut)) for (k,t) in self._trees ]
     def dumpEvents(self,cut,vars=['run','lumi','event']):
         for (k,t) in self._trees:
             print "Dump for channel ",k
@@ -58,13 +80,16 @@ class TreeToYield:
         return yields
     def _getYield(self,tree,cut):
         if self._options.weight:
-            tree.Draw("0.5>>hx(1,0.,1.)","weight*("+cut+")","goff")
-            hx = ROOT.gROOT.FindObject("hx")
-            return hx.GetBinContent(1) 
+            histo = self._getPlot(tree,"0.5","dummy","(1,0.,1.)",cut)
+            return histo.GetBinContent(1) 
         else: 
-            npass = tree.Draw("dphill",cut,"goff");
+            npass = tree.Draw("1",cut,"goff");
             return npass
-    
+    def _getPlot(self,tree,expr,name,bins,cut):
+            if self._options.weight: cut = "weight*("+cut+")"
+            tree.Draw("%s>>%s(%s)" % (expr,name,bins), cut ,"goff")
+            histo = ROOT.gROOT.FindObject(name).Clone(name)
+            return histo
 
 if __name__ == "__main__":
     from optparse import OptionParser
