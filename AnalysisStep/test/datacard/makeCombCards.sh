01287b6fe859982290c84609812ca437596632d1
gpetrucc <>
2011-05-21 18:37:53 +0000

    Pieces to make datacards

---
+++ AnalysisStep/test/datacard/makeCombCards.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+LUMI=$1; if [[ "$LUMI" == "" ]]; then echo "$0 lumi"; exit 1; fi;
+for m in $(cat ../step3/masses); do 
+    for J in 0 1; do
+        echo " make hww-$LUMI.mH$m.comb_${J}j.txt"
+        combineCards.py mm_${J}j=hww-$LUMI.mH$m.mm_${J}j.txt \
+                        me_${J}j=hww-$LUMI.mH$m.me_${J}j.txt \
+                        em_${J}j=hww-$LUMI.mH$m.em_${J}j.txt \
+                        ee_${J}j=hww-$LUMI.mH$m.ee_${J}j.txt \
+                > hww-$LUMI.mH$m.comb_${J}j.txt; 
+    done
+    echo " make hww-$LUMI.mH$m.comb.txt"
+    combineCards.py HWW=hww-$LUMI.mH$m.comb_0j.txt HWW=hww-$LUMI.mH$m.comb_1j.txt > hww-$LUMI.mH$m.comb.txt
+done
