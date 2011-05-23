d116edaaf92146c4a3b1b375e6379f48971669eb
gpetrucc <>
2011-05-23 21:35:54 +0000

    don't combine missing cards

--- AnalysisStep/test/datacard/makeCombCards.sh
+++ AnalysisStep/test/datacard/makeCombCards.sh
@@ -2,6 +2,8 @@
 LUMI=$1; if [[ "$LUMI" == "" ]]; then echo "$0 lumi"; exit 1; fi;
 for m in $(cat ../step3/masses); do 
     for J in 0 1; do
+        test -f hww-$LUMI.mH$m.mm_${J}j.txt || echo "No $J-jets datacards for m(H) = $m, lumi $LUMI (hww-$LUMI.mH$m.mm_${J}j.txt)";
+        test -f hww-$LUMI.mH$m.mm_${J}j.txt || break;
         echo " make hww-$LUMI.mH$m.comb_${J}j.txt"
         combineCards.py mm_${J}j=hww-$LUMI.mH$m.mm_${J}j.txt \
                         me_${J}j=hww-$LUMI.mH$m.me_${J}j.txt \
@@ -9,6 +11,7 @@ for m in $(cat ../step3/masses); do
                         ee_${J}j=hww-$LUMI.mH$m.ee_${J}j.txt \
                 > hww-$LUMI.mH$m.comb_${J}j.txt; 
     done
+    test -f hww-$LUMI.mH$m.comb_0j.txt || continue;
     echo " make hww-$LUMI.mH$m.comb.txt"
     combineCards.py HWW=hww-$LUMI.mH$m.comb_0j.txt HWW=hww-$LUMI.mH$m.comb_1j.txt > hww-$LUMI.mH$m.comb.txt
 done
