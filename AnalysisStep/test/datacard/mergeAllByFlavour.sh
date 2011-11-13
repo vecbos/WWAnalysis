b69228e21b00c058067248bfd37f4249736bcbab
mangano <>
2011-11-13 11:35:20 +0000

    now WJets is correctly merged upstream

--- AnalysisStep/test/datacard/mergeAllByFlavour.sh
+++ AnalysisStep/test/datacard/mergeAllByFlavour.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 MERGE="$CMSSW_BASE/src/WWAnalysis/AnalysisStep/test/datacard/mergeByFlavour.py"
 for J in 0j 1j; do
-    for X in DYCard TopCard WWCard ggWWCard WJet; do
+#    for X in DYCard TopCard WWCard ggWWCard WJet; do
+    for X in DYCard TopCard WWCard ggWWCard; do
         OPTS="--type=gamma"; if [[ "$X" == "WJet" ]]; then OPTS=""; fi;
         python $MERGE $OPTS ${X}_ee_$J.txt ${X}_mm_$J.txt > ${X}_sf_$J.txt && echo ${X}_sf_$J.txt
         if [[ "$X" != "DYCard" ]]; then
