2cdc0e3c921c74b9f5f2897da3174c393a874f43
gpetrucc <>
2012-02-12 18:00:30 +0000

    ZZ4L sequences

--- AnalysisStep/python/zz4lTree_cfi.py
+++ AnalysisStep/python/zz4lTree_cfi.py
@@ -15,17 +15,24 @@ zz4lTree = cms.EDFilter("ProbeTreeProducer",
         z2pt   = cms.string("z(1).pt"),
         elPtMin = cms.string("elePtMin"),
         muPtMin  = cms.string("muPtMin"),
-        pfmet  = cms.string("pfMet.pt()"),
+        pfmet    = cms.string("pfMet.pt()"),
+        worsePairCombRelIsoBaseline = cms.string("worsePairCombRelIsoBaseline()"),
     ),
     flags = cms.PSet(
         # ===========================
         muId11Hits   = cms.string("nGoodLeptons('track.numberOfValidHits > 10', '') == 4"),
         muIdGlbOr2st = cms.string("nGoodLeptons('isGlobalMuon || numberOfMatchedStations > 1', '') == 4"),
-        muIdNominal  = cms.string("nGoodLeptons('isGlobalMuon && track.numberOfValidHits > 10', '') == 4"),
+        muIdBaseline  = cms.string("nGoodLeptons('isGlobalMuon && track.numberOfValidHits > 10', '') == 4"),
+        muIdPF       = cms.string("nGoodLeptons('userInt(\"pfMuId\") > 0', '') == 4"),
         # ===========================
         elIdLoose   = cms.string("nGoodLeptons('', 'test_bit(electronID(\"cicLoose\"),0)  == 1') == 4"),
         elIdMedium  = cms.string("nGoodLeptons('', 'test_bit(electronID(\"cicMedium\"),0) == 1') == 4"),
-        elIdNominal = cms.string("nGoodLeptons('', 'test_bit(electronID(\"cicTight\"),0)  == 1') == 4"),
+        elIdBaseline = cms.string("nGoodLeptons('', 'test_bit(electronID(\"cicTight\"),0)  == 1') == 4"),
+        elIdBDT     = cms.string("nGoodLeptons('', 'abs(eta) < 1.479 && electronID(\"dbBDT\") > -0.11 || "+
+                                                   "abs(eta) > 1.479 && electronID(\"dbBDT\") > -0.05') == 4"),
+        # ===========================
+        #  For the 4e and 4mu final states, at least three of the four combinations of opposite-sign pairs must satisfy m(ll) > 12 GeV
+        threeMassCutBaseline = cms.string("hypo == 4 || hypo == 5 || nGoodPairs(\"mass > 12\", 0) >= 3"),
         # ===========================
         mc_l1 = cms.string("genl(0,0).isNonnull"),
         mc_l2 = cms.string("genl(0,1).isNonnull"),
@@ -53,11 +60,13 @@ addLepVar(zz4lTree, "phi", "lphi(%d,%d)")
 addLepVar(zz4lTree, "q", "lq(%d,%d)")
 addLepVar(zz4lTree, "dz", "ldz(%d,%d)")
 addLepVar(zz4lTree, "ip2d", "lip2d(%d,%d)")
-addLepVar(zz4lTree, "ip3d", "lip3d(%d,%d)")
-addLepVar(zz4lTree, "sip2d", "lsip2d(%d,%d)")
+#addLepVar(zz4lTree, "ip3d", "lip3d(%d,%d)")
+#addLepVar(zz4lTree, "sip2d", "lsip2d(%d,%d)")
 addLepVar(zz4lTree, "sip3d", "lsip3d(%d,%d)")
-addLepVar(zz4lTree, "isoCh",   "userFloat('iso4lCharged_%d_%d')")
-addLepVar(zz4lTree, "isoChNC", "userFloat('iso4lChargedNC_%d_%d')")
-addLepVar(zz4lTree, "isoNeu",  "userFloat('iso4lNeutral_%d_%d')")
-addLepVar(zz4lTree, "isoPho",  "userFloat('iso4lPhotons_%d_%d')")
-addLepVar(zz4lTree, "isoPhoC", "userFloat('iso4lPhotonsC_%d_%d')")
+addLepVar(zz4lTree, "trkIsoBaseline", "lisoTrkBaseline(%d,%d)")
+#addLepVar(zz4lTree, "ecalIsoBaseline", "lisoHcalBaseline(%d,%d)")
+#addLepVar(zz4lTree, "hcalIsoBaseline", "lisoEcalBaseline(%d,%d)")
+addLepVar(zz4lTree, "combRelIsoBaseline", "lisoCombRelBaseline(%d,%d)")
+addLepVar(zz4lTree, "isoCh",  "lisoPf(%d,%d,\"Charged\")")
+addLepVar(zz4lTree, "isoNeu", "lisoPf(%d,%d,\"Neutral\")")
+addLepVar(zz4lTree, "isoPho", "lisoPf(%d,%d,\"Photons\")")
