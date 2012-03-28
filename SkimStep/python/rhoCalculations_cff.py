50b93c612f31a2ad4486eede0fdf7ef47d85a64c
thea <>
2012-03-28 18:35:18 +0000

    some latino collections re-added in order to operate the latinoYieldSkim

--- SkimStep/python/rhoCalculations_cff.py
+++ SkimStep/python/rhoCalculations_cff.py
@@ -1,21 +1,37 @@
 import FWCore.ParameterSet.Config as cms
 
-def addRhoVariables(process,seq,eleTag='gsfElectrons',muTag='muons'):
-    process.load('RecoJets.Configuration.RecoPFJets_cff')
+def addRhoVariables(process,seq,eleTag='gsfElectrons',muTag='muons',pfNoPUTag='pfNoPileUp'):
+
     process.kt6PFJetsForIso = process.kt6PFJets.clone( Rho_EtaMax = cms.double(2.5), Ghost_EtaMax = cms.double(2.5) )
-    process.load("WWAnalysis.Tools.rhoValueMapProducer_cfi")  
+    process.kt6PFJetsNoPU = process.kt6PFJets.clone( src = pfNoPUTag )
+    process.kt6PFJetsForIsoNoPU = process.kt6PFJetsForIso.clone( src = pfNoPUTag )
+
+    # Re-cluster jets w/ pfNoPileUp
+    process.ak5PFJetsNoPU = process.ak5PFJets.clone( src = pfNoPUTag )
+    
+    process.load("WWAnalysis.Tools.rhoValueMapProducer_cfi")
+    
     process.rhoMu = process.rhoValueMapProducer.clone(rhoTag = cms.untracked.InputTag("kt6PFJetsForIso","rho",process.name_()), leptonTag = muTag)
     process.rhoEl = process.rhoMu.clone(leptonTag = eleTag)
-
+    process.rhoMuNoPU = process.rhoValueMapProducer.clone(rhoTag = cms.untracked.InputTag("kt6PFJetsForIsoNoPU","rho",process.name_()), leptonTag = muTag)
+    process.rhoElNoPU = process.rhoMuNoPU.clone(leptonTag = eleTag)
+    
     process.valueMaps = cms.Sequence(
         process.rhoMu +
-        process.rhoEl
+        process.rhoEl +
+        process.rhoMuNoPU +
+        process.rhoElNoPU 
     )
 
     seq += (
         process.ak5PFJets + 
         process.kt6PFJets +
-        process.kt6PFJetsForIso +
+        process.kt6PFJetsForIso + 
+        process.pfPileUp *
+        process.pfNoPileUp * (
+            process.ak5PFJetsNoPU +
+            process.kt6PFJetsNoPU + 
+            process.kt6PFJetsForIsoNoPU ) *
         process.valueMaps 
     )
-        
+
