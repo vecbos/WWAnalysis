f271a1bc2aeed92e24bf6c8b3a956cd95c88e247
gpetrucc <>
2012-04-27 20:59:21 +0000

    Adding SkimEvent2LSelector that was missing from CVS

--- AnalysisStep/plugins/SkimEvent4LSelector.cc
+++ AnalysisStep/plugins/SkimEvent4LSelector.cc
@@ -2,6 +2,7 @@
 #include "CommonTools/UtilAlgos/interface/SingleObjectSelector.h"
 #include "CommonTools/UtilAlgos/interface/StringCutObjectSelector.h"
 #include "WWAnalysis/DataFormats/interface/SkimEvent4L.h"
+#include "WWAnalysis/DataFormats/interface/SkimEvent2L.h"
 
 typedef SingleObjectSelector<
             std::vector<reco::SkimEvent4L>,
@@ -9,4 +10,11 @@ typedef SingleObjectSelector<
             std::vector<reco::SkimEvent4L>
         > SkimEvent4LSelector;
 
+typedef SingleObjectSelector<
+            std::vector<reco::SkimEvent2L>,
+            StringCutObjectSelector<reco::SkimEvent2L>,
+            std::vector<reco::SkimEvent2L>
+        > SkimEvent2LSelector;
+
 DEFINE_FWK_MODULE(SkimEvent4LSelector);
+DEFINE_FWK_MODULE(SkimEvent2LSelector);
