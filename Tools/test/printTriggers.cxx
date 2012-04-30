097d9635f03db2940a7e67fcb8d019efeb0de9f5
gpetrucc <>
2012-04-30 09:14:43 +0000

    updated script to print more info

--- Tools/test/printTriggers.cxx
+++ Tools/test/printTriggers.cxx
@@ -4,13 +4,22 @@
 
     You can specify the process name, and optionally a trigger path name (if you do, only events passing the path are dumped)
 
-    When printing trigger paths associated to an object, it will put a (!) after objects that are from the last filter of the
-    path (if the path is succeeding), and a (-) for intermediate objects (or objects in a path that failed)
+    When printing trigger paths associated to an object, it will put the following postfixes after the object
+        (L,3) if it's considered both a 'Last' filter and a L3 filter ( i.e. if it would match with (1,1) )
+        (*,3) if it's considered a L3 filter ( i.e. if it would match with (0,1), that is the default )
+        (L,*) if it's considered a Last filter ( i.e. if it would match with (1,0) )
+        (*,*) if it matches only with (0,0)
 */
+
+// vvv----- Needed in 5.2.X to avoid choking on C++11 data
+#define __GCCXML__
+// ^^^----- Needed in 5.2.X to avoid choking on C++11 data
+
 #include "DataFormats/FWLite/interface/Handle.h"
 #include "DataFormats/FWLite/interface/Event.h"
 #include <TFile.h>
 #include <iostream>
+#include <exception>
 
 #if !defined(__CINT__) && !defined(__MAKECINT__)
 #include "DataFormats/PatCandidates/interface/Muon.h"
@@ -37,8 +46,15 @@ void dumpTOS( const pat::TriggerObjectStandAloneCollection & objs ) {
         std::vector<std::string> pathNamesLast = o.pathNames(true);
         std::cout << "\t   Paths (" << pathNamesAll.size()<<"/"<<pathNamesLast.size()<<"):    ";
         for (unsigned h = 0, n = pathNamesAll.size(); h < n; ++h) {
-            bool isLast = std::count(pathNamesLast.begin(), pathNamesLast.end(), pathNamesAll[h]);
-            std::cout << " " << pathNamesAll[h] << (isLast?"(!) ":"(-) ");
+            bool isBoth = o.hasPathName( pathNamesAll[h], true, true ); 
+            bool isL3   = o.hasPathName( pathNamesAll[h], false, true ); 
+            bool isLF   = o.hasPathName( pathNamesAll[h], true, false ); 
+            bool isNone = o.hasPathName( pathNamesAll[h], false, false ); 
+            std::cout << "   " << pathNamesAll[h];
+            if (isBoth) std::cout << "(L,3)";
+            if (isL3 && !isBoth) std::cout << "(*,3)";
+            if (isLF && !isBoth) std::cout << "(L,*)";
+            if (isNone && !isBoth && !isL3 && !isLF) std::cout << "(*,*)";
         }
         std::cout << std::endl;
     }
@@ -64,7 +80,7 @@ void printTriggers(const char *process = "HLT", const char *path="") {
             if (index == triggerNames.size()) {  std::cerr << "Trigger " << path << " not found." << std::endl; continue; }
             if (!hTriggerResults->accept(index)) continue;
         }
-        std::cout << "\n\n\n === EVENT " << iEvent << ": HLT RESULTS ====" << std::endl;
+        std::cout << "\n\n\n === EVENT " << iEvent << "(run " << ev.id().run() << ", ls " << ev.id().luminosityBlock() << ", event " << ev.id().event() << "): HLT RESULTS ====" << std::endl;
         for (unsigned i = 0; i < triggerNames.size(); ++i) {
             if (strstr(triggerNames.triggerName(i).c_str(), "Mu") || 
                 strstr(triggerNames.triggerName(i).c_str(), "Ele")) {
@@ -81,21 +97,25 @@ void printTriggers(const char *process = "HLT", const char *path="") {
 
         std::cout << " === EVENT " << iEvent << ": Muons ====" << std::endl;
         hMu.getByLabel(ev,"boostedMuons");
-        const std::vector<pat::Muon>     & mus = *hMu;
-        for (unsigned i = 0; i < mus.size(); ++i) {
-            const pat::Muon &mu = mus[i];
-            std::cout << "  mu " << i << ", pt " << mu.pt() << ", eta " << mu.eta() << std::endl;
-            dumpTOS(mu.triggerObjectMatches());
-        } // muon  
+        if (!hMu.failedToGet()) {
+            const std::vector<pat::Muon>     & mus = *hMu;
+            for (unsigned i = 0; i < mus.size(); ++i) {
+                const pat::Muon &mu = mus[i];
+                std::cout << "  mu " << i << ", pt " << mu.pt() << ", eta " << mu.eta() << std::endl;
+                dumpTOS(mu.triggerObjectMatches());
+            } // muon  
+        }
 
         std::cout << " === EVENT " << iEvent << ": Electrons ====" << std::endl;
         hEle.getByLabel(ev,"boostedElectrons");
-        const std::vector<pat::Electron> & els = *hEle;
-        for (unsigned i = 0; i < els.size(); ++i) {
-            const pat::Electron &el = els[i];
-            std::cout << "  el " << i << ", pt " << el.pt() << ", eta " << el.eta() << std::endl;
-            dumpTOS(el.triggerObjectMatches());
-        } // el
+        if (!hEle.failedToGet()) {
+            const std::vector<pat::Electron> & els = *hEle;
+            for (unsigned i = 0; i < els.size(); ++i) {
+                const pat::Electron &el = els[i];
+                std::cout << "  el " << i << ", pt " << el.pt() << ", eta " << el.eta() << std::endl;
+                dumpTOS(el.triggerObjectMatches());
+            } // el
+        }
         if (iEvent >= 20) break;
     } // event
 #endif
