7055d2656322f0b00d830e4d14235fe2a3b99d07
thea <>
2012-05-14 10:25:56 +0000

    Bugfix: triggering mva was given the ipsignificance in place of ip. Fixed.

--- AnalysisStep/plugins/PatElectronBoosterBDTID.cc
+++ AnalysisStep/plugins/PatElectronBoosterBDTID.cc
@@ -92,6 +92,9 @@ PatElectronBoosterBDTID::PatElectronBoosterBDTID(const edm::ParameterSet& iConfi
   eleMVANonTrig  = new EGammaMvaEleEstimator();
   eleMVATrig->initialize("BDT",EGammaMvaEleEstimator::kTrig,true,manualCatTrigWeigths);
   eleMVANonTrig->initialize("BDT",EGammaMvaEleEstimator::kNonTrig,true,manualCatNonTrigWeigths);
+  
+  eleMVATrig->SetPrintMVADebug(kTRUE);
+//   eleMVANonTrig->SetPrintMVADebug(kTRUE);
 
 
   // ---------
@@ -117,8 +120,7 @@ void PatElectronBoosterBDTID::produce(edm::Event& iEvent, const edm::EventSetup&
     for(edm::View<reco::Candidate>::const_iterator ele=electrons->begin(); ele!=electrons->end(); ++ele){    
       const pat::ElectronRef elecsRef = edm::RefToBase<reco::Candidate>(electrons,ele-electrons->begin()).castTo<pat::ElectronRef>();
       pat::Electron clone = *edm::RefToBase<reco::Candidate>(electrons,ele-electrons->begin()).castTo<pat::ElectronRef>();
-      
-      
+
       // ------ HERE I ADD THE BDT ELE ID VALUE TO THE ELECTRONS
       double xieSign  = ( (-clone.userFloat("dxyPV")) >=0 )  ? 1: -1;
       float ctfChi2 = clone.userFloat("ctfChi2"); if (ctfChi2 == -1) ctfChi2 = 0; // fix a posteriori problem in booster
@@ -141,8 +143,8 @@ void PatElectronBoosterBDTID::produce(edm::Event& iEvent, const edm::EventSetup&
                      (1.0 / clone.ecalEnergy()) - (1.0 / clone.p()),   
                      clone.eEleClusterOverPout(),
                      clone.superCluster()->preshowerEnergy()/clone.superCluster()->rawEnergy(),          
-					 xieSign*clone.userFloat("dxyPV"),
-					 xieSign*clone.userFloat("ip")/clone.userFloat("ipErr"),
+					 -clone.userFloat("dxyPV"),
+					 xieSign*clone.userFloat("ip"),
                      clone.superCluster()->eta(), 
                      clone.pt(), 
                      debug_);	
@@ -173,7 +175,6 @@ void PatElectronBoosterBDTID::produce(edm::Event& iEvent, const edm::EventSetup&
       clone.addUserFloat(std::string("bdtnontrig"+postfix_),mvaValueNonTrig);
 
       // -----------------------------
-      
 
       pOut->push_back(clone);
 	
