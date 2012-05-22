05bde004ebe2cd109a125969bcfbd603db0a79ae
gpetrucc <>
2012-05-22 21:45:26 +0000

    Fix inner cone veto for Muon PF Iso

--- Tools/python/muonPFIsoSingleType_cfi.py
+++ Tools/python/muonPFIsoSingleType_cfi.py
@@ -5,6 +5,7 @@ muonPFIsoSingleTypeMapProd = cms.EDProducer('MuonPFIsoSingleTypeMapProd',
                                             vtxLabel = cms.untracked.InputTag("offlinePrimaryVertices"),
                                             pfLabel = cms.untracked.InputTag("pfNoPileUp"),
                                             deltaR = cms.untracked.double(0.4),
+                                            innerDeltaRVeto = cms.untracked.double(0.0),
                                             pfTypes = cms.untracked.vint32(1, 2, 3, 4, 5),
                                             directional = cms.untracked.bool(False)
                                             )
