0d6bb88926dbb9d08f1f27c9b2c2892c44fa4b5a
gpetrucc <>
2012-05-28 17:45:11 +0000

    More photon isolation options

--- Tools/python/fsrPhotons_cff.py
+++ Tools/python/fsrPhotons_cff.py
@@ -17,6 +17,10 @@ fsrPhotonPFIsoChHad04 = cms.EDProducer("LeptonPFIsoFromStep1",
     vetoConeEndcaps = cms.double(0.0), # no special veto in the endcaps
     directional = cms.bool(False),
 )
+# Separate version with Patrick's threshold
+fsrPhotonPFIsoChHad04pt02 = fsrPhotonPFIsoChHad04.clone(
+    pfSelection = fsrPhotonPFIsoChHad04.pfSelection.value() + " && pt > 0.2"
+)
 fsrPhotonPFIsoNHad04 = fsrPhotonPFIsoChHad04.clone(
     pfSelection = "charge == 0 && abs(pdgId) == 130 && pt > 0.5", # neutral hadrons
     deltaRself  = 0.01, # larger veto cone for neutrals
@@ -26,9 +30,17 @@ fsrPhotonPFIsoPhoton04 = fsrPhotonPFIsoChHad04.clone(
     deltaRself  = 0.01, # larger veto cone for neutrals
 )
 # for deltaBeta corrections
-fsrPhotonPFIsoChHadPU04 = fsrPhotonPFIsoChHad04.clone(
-    pfLabel = 'pfPileUp'
-)
+fsrPhotonPFIsoChHadPU04 = fsrPhotonPFIsoChHad04.clone(pfLabel = 'pfPileUp')
+fsrPhotonPFIsoChHadPU04pt02 = fsrPhotonPFIsoChHad04pt02.clone(pfLabel = 'pfPileUp')
+
+## deltaR = 0.3 copy
+fsrPhotonPFIsoChHad03       = fsrPhotonPFIsoChHad04.clone(deltaR = 0.3)
+fsrPhotonPFIsoChHad03pt02   = fsrPhotonPFIsoChHad04pt02.clone(deltaR = 0.3)
+fsrPhotonPFIsoNHad03        = fsrPhotonPFIsoNHad04.clone(deltaR = 0.3)
+fsrPhotonPFIsoPhoton03      = fsrPhotonPFIsoPhoton04.clone(deltaR = 0.3)
+fsrPhotonPFIsoChHadPU03     = fsrPhotonPFIsoChHadPU04.clone(deltaR = 0.3)
+fsrPhotonPFIsoChHadPU03pt02 = fsrPhotonPFIsoChHadPU04pt02.clone(deltaR = 0.3)
+
 # for rho corrections
 import WWAnalysis.Tools.rhoValueMapProducer_cfi
 rhoFsrPhoton = WWAnalysis.Tools.rhoValueMapProducer_cfi.rhoValueMapProducer.clone(
@@ -43,18 +55,34 @@ boostedFsrPhotons = PhysicsTools.PatAlgos.producersLayer1.pfParticleProducer_cfi
 )
 boostedFsrPhotons.userData.userFloats.src = cms.VInputTag(
     cms.InputTag("fsrPhotonPFIsoChHad04"),
+    cms.InputTag("fsrPhotonPFIsoChHad04pt02"),
     cms.InputTag("fsrPhotonPFIsoNHad04"),
     cms.InputTag("fsrPhotonPFIsoPhoton04"),
     cms.InputTag("fsrPhotonPFIsoChHadPU04"),
+    cms.InputTag("fsrPhotonPFIsoChHadPU04pt02"),
+    cms.InputTag("fsrPhotonPFIsoChHad03"),
+    cms.InputTag("fsrPhotonPFIsoChHad03pt02"),
+    cms.InputTag("fsrPhotonPFIsoNHad03"),
+    cms.InputTag("fsrPhotonPFIsoPhoton03"),
+    cms.InputTag("fsrPhotonPFIsoChHadPU03"),
+    cms.InputTag("fsrPhotonPFIsoChHadPU03pt02"),
     cms.InputTag("rhoFsrPhoton"),
 )
 
 fsrPhotonSequence = cms.Sequence(
     fsrPhotons +
     fsrPhotonPFIsoChHad04 + 
+    fsrPhotonPFIsoChHad04pt02 + 
     fsrPhotonPFIsoNHad04 + 
     fsrPhotonPFIsoPhoton04 + 
     fsrPhotonPFIsoChHadPU04 + 
+    fsrPhotonPFIsoChHadPU04pt02 + 
+    fsrPhotonPFIsoChHad03 + 
+    fsrPhotonPFIsoChHad03pt02 + 
+    fsrPhotonPFIsoNHad03 + 
+    fsrPhotonPFIsoPhoton03 + 
+    fsrPhotonPFIsoChHadPU03 + 
+    fsrPhotonPFIsoChHadPU03pt02 + 
     rhoFsrPhoton + 
     boostedFsrPhotons
 )
