c883975186142c62b136362a0d3610fd0ad9bab1
gpetrucc <>
2012-06-03 13:20:20 +0000

    Switch from deltaBeta to withPU iso

--- AnalysisStep/python/zz4l/fsr_cff.py
+++ AnalysisStep/python/zz4l/fsr_cff.py
@@ -21,6 +21,9 @@ fsrPhotonsNoEleSC = cms.EDProducer("PATPFParticleCleaner",
 _netural_iso_uncorr = "pf.userFloat('fsrPhotonPFIsoPhoton03') + pf.userFloat('fsrPhotonPFIsoNHad03')"
 _neutral_iso_dbeta  = "max(0, %s - 0.5*pf.userFloat('fsrPhotonPFIsoChHadPU03pt02'))" % _netural_iso_uncorr
 _comb_rel_iso_dbeta = "(pf.userFloat('fsrPhotonPFIsoChHad03pt02') + %s)/pf.pt" % _neutral_iso_dbeta
+## Definition of PF relative isolation including pileup
+_comb_iso_all = "+".join(["pf.userFloat('%s')"%x for x in ('fsrPhotonPFIsoChHad03pt02','fsrPhotonPFIsoPhoton03','fsrPhotonPFIsoNHad03','fsrPhotonPFIsoChHadPU03pt02')])
+_comb_rel_iso_all = "(%s)/pf.pt" % _comb_iso_all
 
 fsrPhotons = cms.EDProducer("PATPFParticleCleaner",
     src = cms.InputTag("fsrPhotonsNoEleSC"),
@@ -28,6 +31,16 @@ fsrPhotons = cms.EDProducer("PATPFParticleCleaner",
     checkOverlaps = cms.PSet(
         ## Attach by deltaR, pT and isolation
         goodLepIso = cms.PSet(
+            src = cms.InputTag("goodLep"),
+            algorithm = cms.string("byDeltaR"),
+            preselection = cms.string(""),
+            pairCut = cms.string("deltaR < 0.07 || (pf.pt > 4 && %s < 1.0)" % _comb_rel_iso_all),
+            deltaR = cms.double(0.5),
+            checkRecoComponents = cms.bool(False),
+            requireNoOverlaps   = cms.bool(False),
+        ),
+        ## Attach by deltaR, pT and isolation with deltabeta (old)
+        goodLepIsoDBeta = cms.PSet(
             src = cms.InputTag("goodLep"),
             algorithm = cms.string("byDeltaR"),
             preselection = cms.string(""),
