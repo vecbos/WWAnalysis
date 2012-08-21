d1d5a646063e9328041fe91ad6b310984c58fa57
sixie <>
2012-08-21 21:07:58 +0000

    add regression electron producer

---
+++ AnalysisStep/python/regressionEnergyPatElectrons_cfi.py
@@ -0,0 +1,24 @@
+
+import FWCore.ParameterSet.Config as cms
+
+
+#==============================================================================
+# regreesion energy gsf electrons
+#==============================================================================
+
+regressionEnergyPatElectrons = cms.EDProducer("RegressionEnergyPatElectronProducer",
+                                              
+    # input collections
+    inputPatElectronsTag = cms.InputTag("boostedElectrons"),
+
+    # set to True to get debugging printout   
+    debug = cms.bool(True),
+                                              
+    #regression energy type
+    energyRegressionType = cms.untracked.uint32(1),
+
+    #regression weight file                                              
+    regressionInputFile = cms.string("weightFile_V11.root")
+)
+
+
