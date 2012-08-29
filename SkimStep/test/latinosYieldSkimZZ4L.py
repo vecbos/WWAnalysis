a6f2731edca426717e1bbd957b84d73e901e34f3
mangano <>
2012-08-29 16:39:43 +0000

    changes for 53X release

--- SkimStep/test/latinosYieldSkimZZ4L.py
+++ SkimStep/test/latinosYieldSkimZZ4L.py
@@ -65,7 +65,7 @@ elif releaseVer == "53X" :
         process.GlobalTag.globaltag = 'START53_V10::All'   #for 53X MC  
     else:
         process.GlobalTag.globaltag = 'FT_53_V6C_AN1::All'   #for 53X DATA July13 ReReco
-        process.GlobalTag.globaltag = 'GR_P_V41_AN1::All'   #for 2012C prompt-data and >=533 release
+        #process.GlobalTag.globaltag = 'GR_P_V41_AN1::All'   #for 2012C prompt-data and >=533 release
 
 
 process.source = cms.Source("PoolSource", 
