65c47adfabcfd2b197bed39829a24a0885efaaa0
avartak <>
2012-09-28 07:57:59 +0000

    Adding new PDFs for signal shape

--- AnalysisStep/src/LinkDef.h
+++ AnalysisStep/src/LinkDef.h
@@ -1,5 +1,8 @@
 #include "RooXZsZs_5D.h"
 #include "RooqqZZ_JHU.h"
+#include "RooCruijff.h"
+#include "RooCrystalCruijff.h"
+#include "RooDoubleCB.h"
 
 #ifdef __CINT__
 #pragma link off all globals;
@@ -7,4 +10,7 @@
 #pragma link off all functions;
 #pragma link C++ class RooXZsZs_5D+;
 #pragma link C++ class RooqqZZ_JHU+;
+#pragma link C++ class RooCruijff+;
+#pragma link C++ class RooCrystalCruijff+;
+#pragma link C++ class RooDoubleCB+;
 #endif
