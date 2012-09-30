b5c436caff885d9e170bc4078908b6b9f2586b67
emanuele <>
2012-09-30 15:27:31 +0000

    update the name of the 2D histo

--- TreeModifiers/src/LeptSfProvider.cc
+++ TreeModifiers/src/LeptSfProvider.cc
@@ -249,7 +249,7 @@ void LeptSfProvider::initEl(bool is2011) {
     TFile file(filepath.c_str());
 
 
-    electronscalefactors = (TH2*)file.Get("heff");
+    electronscalefactors = (TH2*)file.Get("heff_electron_selection");
 
     if (elesf.size() == 0) {
         for (std::size_t i = 0; i < 7; i++) {
