c013b927b67b91e4ab42edbc848d11c2965879ea
avartak <>
2012-10-01 14:55:59 +0000

    Removing PDFs and putting them in TreeModifiers

---
+++ TreeModifiers/src/LinkDef.h
@@ -0,0 +1,12 @@
+#include "RooCruijff.h"
+#include "RooCrystalCruijff.h"
+#include "RooDoubleCB.h"
+
+#ifdef __CINT__
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link C++ class RooCruijff+;
+#pragma link C++ class RooCrystalCruijff+;
+#pragma link C++ class RooDoubleCB+;
+#endif
