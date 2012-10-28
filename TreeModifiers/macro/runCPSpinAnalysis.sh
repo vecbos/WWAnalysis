20b31743226cef997dec4c07cafce3d648472788
sixie <>
2012-10-28 15:34:58 +0000

    update

--- TreeModifiers/macro/runCPSpinAnalysis.sh
+++ TreeModifiers/macro/runCPSpinAnalysis.sh
@@ -1,10 +1,21 @@
 
-
 ############################
 #Make Some Plots
 ############################
 root -l WWAnalysis/TreeModifiers/macro/rootlogon.C WWAnalysis/TreeModifiers/macro/MakeMELAPlots.C+
 
+
+############################################
+#Create Signal Efficiency
+#Need to do it for multiple subchannels
+############################################
+root -l WWAnalysis/TreeModifiers/macro/rootlogon.C WWAnalysis/TreeModifiers/macro/yieldAfterMelaCutFit.C+
+
+
+############################################
+#Compute Bkg Yields and Bkg shapes
+############################################
+root -l WWAnalysis/TreeModifiers/macro/rootlogon.C WWAnalysis/TreeModifiers/macro/getParams.C+
 
 
 ############################
@@ -17,12 +28,6 @@ root -l WWAnalysis/TreeModifiers/macro/rootlogon.C WWAnalysis/TreeModifiers/macr
 
 
 
-############################
-#Calculate Bkg Model parameters
-############################
-root -l WWAnalysis/TreeModifiers/macro/rootlogon.C WWAnalysis/TreeModifiers/macro/getParams.C+
-
-
 
 ############################
 #Make Data Cards
@@ -30,7 +35,7 @@ root -l WWAnalysis/TreeModifiers/macro/rootlogon.C WWAnalysis/TreeModifiers/macr
 root -l WWAnalysis/TreeModifiers/macro/rootlogon.C WWAnalysis/TreeModifiers/macro/makeSpinCPMeasurementDatacards.C+'(1)'
 
 #combine cards
-combineCards.py hzz4l_4mu_7=card_pseudoscalar_2D_m125_7TeV_4mu.txt  hzz4l_4e_7=card_pseudoscalar_2D_m125_7TeV_4e.txt   hzz4l_2e2mu_7=card_pseudoscalar_2D_m125_7TeV_2e2mu.txt  hzz4l_4mu_8=card_pseudoscalar_2D_m125_8TeV_4mu.txt  hzz4l_4e_8=card_pseudoscalar_2D_m125_8TeV_4e.txt     hzz4l_2e2mu_8=card_pseudoscalar_2D_m125_8TeV_2e2mu.txt   > card_pseudoscalar_2D_m125_comb.txt
+combineCards.py hzz4l_4mu_7=card_pseudoscalar_2D_m125_7TeV_4mu.txt  hzz4l_4e_7=card_pseudoscalar_2D_m125_7TeV_4e.txt   hzz4l_2e2mu_7=card_pseudoscalar_2D_m125_7TeV_2e2mu.txt  hzz4l_4mu_8=card_pseudoscalar_2D_m125_8TeV_4mu.txt  hzz4l_4e_8=card_pseudoscalar_2D_m125_8TeV_4e.txt     hzz4l_2e2mu_8=card_pseudoscalar_2D_m125_8TeV_2e2mu.txt   >! hzz4lpseudoscalar.text
 
 
 
@@ -42,7 +47,7 @@ combineCards.py hzz4l_4mu_7=card_pseudoscalar_2D_m125_7TeV_4mu.txt  hzz4l_4e_7=c
 ################
 #Make Workspace
 ################
-text2workspace.py -m 125 card_pseudoscalar_2D_m125_comb.txt -P HiggsAnalysis.CombinedLimit.HiggsJPC:twoHypothesisHiggs -o workspace_HCP_pseudoscalar_fixedMu.root
+text2workspace.py -m 125 /afs/cern.ch/work/s/sixie/public/HZZ4l/cards/HCP/pseudoscalar/v4/hzz4lpseudoscalar.text -P HiggsAnalysis.CombinedLimit.HiggsJPC:twoHypothesisHiggs -o workspace_HCP_pseudoscalar_fixedMu.root
 
 
 
@@ -73,5 +78,25 @@ combine -M MultiDimFit workspace_HCP_pseudoscalar_floatMu.root --algo=grid --poi
 combine -M MultiDimFit /afs/cern.ch/work/s/sixie/public/HZZ4l/cards/HCP/pseudoscalar/v1/workspace_HCP_pseudoscalar_floatMu.root --algo=grid --points 100 --fastScan  -m 125 -v 2 -n 1D
 
 
+combine -M MultiDimFit /afs/cern.ch/work/s/sixie/public/HZZ4l/cards/HCP/pseudoscalar/v1/workspace_HCP_pseudoscalar_floatMu.root --algo=grid --points 100 --fastScan  -m 125 --setPhysicsModelParameters "x=0.5" -t -1 -v2 -n 1D 
+
+
+
 #2D scan
 combine -M MultiDimFit /afs/cern.ch/work/s/sixie/public/HZZ4l/cards/HCP/pseudoscalar/v1/tworkspace_HCP_pseudoscalar_woD.root --algo=grid --points 10000 --fastScan  -m 125 -v 2
+
+
+################
+#Build qmu from toys
+################
+
+foreach f(`seq 1 1 400`)
+   root -q -b higgsCombineTest.HybridNew.mH125.${f}.root ${CMSSW_BASE}/src/HiggsAnalysis/CombinedLimit/test/plotting/hypoTestResultTree.cxx\(\"qmu.FixedMu.${f}.root\",125,1,\"x\"\)
+end
+
+
+################
+#Plot Signal Separation
+################
+root -l WWAnalysis/TreeModifiers/macro/rootlogon.C WWAnalysis/TreeModifiers/macro/MakePSHiggsHypTestPlots.C+ 
+
