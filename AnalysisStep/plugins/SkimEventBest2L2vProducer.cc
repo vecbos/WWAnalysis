1abab3651fb057489494ace5c4df012395c52565
amassiro <>
2012-12-19 10:15:05 +0000

    bug fix

--- AnalysisStep/plugins/SkimEventBest2L2vProducer.cc
+++ AnalysisStep/plugins/SkimEventBest2L2vProducer.cc
@@ -13,7 +13,7 @@
 //
 // Original Author:  
 //         Created:  Fri Nov 23 18:14:42 CET 2012
-// $Id: SkimEventBest2L2vProducer.cc,v 1.2 2012/12/13 09:47:11 amassiro Exp $
+// $Id: SkimEventBest2L2vProducer.cc,v 1.3 2012/12/14 09:13:27 amassiro Exp $
 //
 //
 
@@ -128,11 +128,14 @@ SkimEventBest2L2vProducer::produce(edm::Event& iEvent, const edm::EventSetup& iS
     iEvent.getByLabel(mumuTag_,mumu);
 
 
+    std::cout << " mumu = " << mumu->size() << ";  muel = " << muel->size() << "; elmu = " << elmu->size() << "; elel = " << elel->size() << std::endl;
+
     //---- importance order : mm, mu, em, ee ----
     const reco::SkimEvent* best = 0x0;
     for (size_t i=0; i<mumu->size(); ++i) {//---- mm
      const reco::SkimEvent* candidate =  mumu->ptrAt(i).get();
      if ( !best ) { 
+      std::cout << " mumu " << std::endl;
       best = candidate;
       continue;
      } else {
@@ -144,6 +147,7 @@ SkimEventBest2L2vProducer::produce(edm::Event& iEvent, const edm::EventSetup& iS
     for (size_t i=0; i<muel->size(); ++i) {//---- me
      const reco::SkimEvent* candidate =  muel->ptrAt(i).get();
      if ( !best ) { 
+      std::cout << " muel " << std::endl;
       best = candidate;
       continue;
      } else {
@@ -155,6 +159,7 @@ SkimEventBest2L2vProducer::produce(edm::Event& iEvent, const edm::EventSetup& iS
     for (size_t i=0; i<elmu->size(); ++i) {//---- em
      const reco::SkimEvent* candidate =  elmu->ptrAt(i).get();
      if ( !best ) { 
+      std::cout << " elmu " << std::endl;
       best = candidate;
       continue;
      } else {
@@ -162,16 +167,17 @@ SkimEventBest2L2vProducer::produce(edm::Event& iEvent, const edm::EventSetup& iS
       double cpt = (candidate->pt(0)+candidate->pt(1));
       best = ( bpt > cpt ? best : candidate);
      }
-     for (size_t i=0; i<elel->size(); ++i) {//---- ee
-      const reco::SkimEvent* candidate =  elel->ptrAt(i).get();
-      if ( !best ) { 
-       best = candidate;
-       continue;
-      } else {
-       double bpt = (best->pt(0)+best->pt(1));
-       double cpt = (candidate->pt(0)+candidate->pt(1));
-       best = ( bpt > cpt ? best : candidate);
-      }
+    }
+    for (size_t i=0; i<elel->size(); ++i) {//---- ee
+     const reco::SkimEvent* candidate =  elel->ptrAt(i).get();
+     if ( !best ) {
+      std::cout << " elel " << std::endl;
+      best = candidate;
+      continue;
+     } else {
+      double bpt = (best->pt(0)+best->pt(1));
+      double cpt = (candidate->pt(0)+candidate->pt(1));
+      best = ( bpt > cpt ? best : candidate);
      }
     }
 
